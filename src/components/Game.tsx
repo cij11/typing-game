debc32e92322e94de8d5493bf18d0229ac0e37c8
Christopher Jolly <chris.jolly@bachcare.co.nz>
2022-01-18 09:33:33 +1300

    Red border when near death

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -14,10 +14,29 @@ interface Props {
 
 export default class Game extends React.Component<Props> {
     render() {
+        let borderColor = '#000000'
+
+        if (this.props.stack.length > 7) {
+            borderColor = '#FF5F00'
+        }
+
+        if (this.props.stack.length > 8) {
+            borderColor = '#FF0000'
+        }
+
+        const borderStyle = {
+            borderStyle: 'solid',
+            borderColor: borderColor,
+            borderWidth: '5px'
+        }
+
         return (
             <div onKeyDown={this.props.onKeyDown}>
                 <div className="uk-container-large uk-flex">
-                    <div className="uk-width-1-2 uk-card uk-card-secondary uk-card-body">
+                    <div
+                        style={borderStyle}
+                        className="uk-width-1-2 uk-card uk-card-secondary uk-card-body"
+                    >
                         <Stack stack={this.props.stack} />
                     </div>
                     <div className="uk-width-1-2 uk-card uk-card-primary uk-card-body">
